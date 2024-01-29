572b4520943c030155101fd80e51e70fae59bee5
Waqaze <153550986+Waqaze@users.noreply.github.com>
2024-01-29 10:48:15 -0800

    JS-Assignment-1
    JS-Assignment-1

---
+++ app.js
@@ -0,0 +1,304 @@
+window.alert("A-1: Welcome to Our Website");
+
+document.write("A-2: Error! Please Enter a valid password");
+
+document.write("<br>");
+
+document.write("<br>");
+
+document.write("A-3: Hint : Use line break");
+
+window.alert("A-4: Error! Please Enter a valid password");
+
+window.alert("Hint : Use line break");
+
+document.write("<br>");
+
+document.write("<br>");
+
+
+var fullname = ("A-7: Username");
+document.write(fullname);
+
+document.write("<br>");
+
+var myName = ("A-8: Waqas Azeem");
+document.write(myName);
+
+var FullName = ("A-9: Message :" + " " + " Hello! World");
+window.alert(FullName);
+
+document.write("<br>");
+
+document.write("<br>");
+
+var Fullname = ("A-10: Name : Waqas Azeem");
+window.alert(Fullname);
+document.write(Fullname);
+
+document.write("<br>");
+
+var Fathername = ("Father's Name : M.Amin");
+window.alert(Fathername);
+document.write(Fathername);
+
+document.write("<br>");
+
+var Qualification = ("Qualification : Bachelor in Commerce");
+window.alert(Qualification);
+document.write(Qualification);
+
+document.write("<br>");
+
+var Email = ("A-11: email : waqaze@gmail.com");
+window.alert(Email);
+document.write(Email);
+
+document.write("<br>");
+
+window.alert("A-12: Hint: use string concatenation");
+
+var Myname = ("A-13: ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+window.alert(Myname);
+document.write(Myname);
+
+document.write("<br>");
+
+document.write("<br>");
+
+var age = ("A-14: 36 yrs");
+window.alert(age);
+
+var Visitedthispage = ("A-15: You have visited this site 999999999999 times");
+document.write(Visitedthispage);
+
+document.write("<br>");
+
+var birthyear = ("A-16: 1986");
+document.write(birthyear);
+
+document.write("<br>");
+
+document.write("<br>");
+
+var visitorname = ("A-17, 18: Visitor's Name : Syed Afaq Ahmed    /   Product Title : Couture    /    Quantity : 2 Pcs");
+document.write(visitorname);
+
+document.write("<br>");
+
+document.write("John Doe ordered 5 T-shirts on XYZ Clothing store");
+
+document.write("<br>");
+
+document.write("<br>");
+
+document.write("A-20: a) “Rules for naming JS variables”");
+
+document.write("<br>");
+
+document.write("b) Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable");
+
+document.write("<br>");
+
+document.write("c) Variables must begin with a ______, ______ or _____. For example $name, _name or name");
+
+document.write("<br>");
+
+document.write("d) Variable names are case _________");
+
+document.write("<br>");
+
+document.write("e) Variable names should not be JS _________");
+
+document.write("<br>");
+
+document.write("<br>");
+
+document.write("A-21:");
+
+document.write("<br>");
+
+var num1 = (5);
+var num2 = (2);
+document.write(num1 + num2);
+
+document.write("<br>");
+
+document.write("<br>");
+
+document.write("A-22:");
+
+document.write("<br>");
+
+document.write(num1 - num2);
+
+document.write("<br>");
+
+document.write(num1 * num2);
+
+document.write("<br>");
+
+document.write(num1 / num2);
+
+document.write("<br>");
+
+document.write(num1 % num2);
+
+document.write("<br>");
+
+document.write("<br>");
+
+document.write("A-23:");
+
+document.write("<br>");
+
+document.write("a)Declare a variable");
+var num1 = (1);
+
+document.write("<br>");
+
+document.write("b)Value after variable declaration is: 1");
+
+document.write("<br>");
+
+document.write("c)Initialize the variable with some number");
+var num2 = (5);
+
+document.write("<br>");
+
+document.write("d)Initial Value: 5");
+
+document.write("<br>");
+
+document.write("e)Increment the variable");
+
+document.write("<br>");
+
+document.write("f)Value after increment:");
+
+document.write("<br>");
+document.write(num1 + num2);
+
+document.write("<br>");
+
+document.write("g)Add 7 to the variable");
+var num3 = (7);
+
+document.write("<br>");
+
+document.write("h)Value after addition:");
+
+document.write("<br>");
+document.write(num1 + num2 + num3);
+
+document.write("<br>");
+
+document.write("i)Decrement the variable");
+var num4 = (1);
+
+document.write("<br>");
+document.write("j)Value after decrement:");
+
+document.write("<br>");
+document.write(num1 + num2 + num3 - num4);
+
+document.write("<br>");
+document.write("k)dividing the variable's value by 3:");
+var num5 = (12);
+
+document.write("<br>");
+document.write(num5 / num3);
+
+document.write("<br>");
+document.write("l)Output : The remainder is");
+
+document.write("<br>");
+document.write(0);
+
+document.write("<br>");
+
+document.write("<br>");
+
+document.write("A-24:");
+
+document.write("<br>");
+
+document.write("Cost of a movie TKT is 600 PKR:");
+var num1 = (600);
+
+document.write("<br>");
+document.write("Cost of 5 movie TKTs in PKR is");
+var num2 = (5);
+
+document.write("<br>");
+document.write(num1 * num2);
+
+document.write("<br>");
+
+document.write("<br>");
+
+document.write("A-25:");
+
+document.write("<br>");
+document.write("<br>");
+
+document.write("Table of 2");
+
+document.write("<br>");
+document.write("<br>");
+
+var num1 = (2);
+var num2 = (1);
+var num3 = (2);
+var num4 = (3);
+var num5 = (4);
+var num6 = (5);
+var num7 = (6);
+var num8 = (7);
+var num9 = (8);
+var num10 = (9);
+var num11 = (10);
+document.write("2x1=");
+document.write(num1 * num2);
+
+document.write("<br>");
+document.write("2x2=");
+document.write(num1 * num3);
+
+document.write("<br>");
+document.write("2x3=");
+document.write(num1 * num4);
+
+document.write("<br>");
+document.write("2x4=");
+document.write(num1 * num5);
+
+document.write("<br>");
+document.write("2x5=");
+document.write(num1 * num6);
+
+document.write("<br>");
+document.write("2x6=");
+document.write(num1 * num7);
+
+document.write("<br>");
+document.write("2x7=");
+document.write(num1 * num8);
+
+document.write("<br>");
+document.write("2x8=");
+document.write(num1 * num9);
+
+document.write("<br>");
+document.write("2x9=");
+document.write(num1 * num10);
+
+document.write("<br>");
+document.write("2x10=");
+document.write(num1 * num11);
+
+document.write("<br>");
+document.write("<br>");
+
+var Myname = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+document.write(Myname);
